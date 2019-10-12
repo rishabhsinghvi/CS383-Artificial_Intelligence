8abb184ab09e0bfc706f9092862863f135cae71f
Rishabh Singhvi <rdpsinghvi@gmail.com>
2019-10-12 16:04:02 -0400

    improvement to scheduling csp

--- Assignment_2/scheduling.py
+++ Assignment_2/scheduling.py
@@ -1,20 +1,11 @@
 from constraint import *
 
-courses = [234, 248, 388, 622, 400, 457, 126, 383, 635]
 
 # Requirements
 air = [[234, 248], [234, 388], [622, 457]]
 adver = [[388], [622], [400]]
 core = [[457], [383], [126]]
 malic = [[388], [635]]
-
-# Can't take too many
-too_evil = [388, 622, 635]
-wizard_basic = [234, 126]
-witchcraft = [457, 383]
-
-# Already taken
-taken = [635, 234]
 
 
 # Constraint to guarantee no course is taken for multiple requirements
@@ -34,55 +25,49 @@ def unique_courses(a, b, c, d):
 
 
 
-def good_wizard(a, b, c, d):
-    
+def good_wizard_restriction(a, b, c, d):   
     list = set([388, 622, 635])
-
     return len(list & set(a)) + len(list & set(b)) + len(list & set(c)) + len(list & set(d)) in [0, 1]
     
 
         
-def wizardry_basic(a, b, c, d):
-    
+def wizardry_basic_restriction(a, b, c, d):
     list = set([234, 126])
-
     return len(list & set(a)) + len(list & set(b)) + len(list & set(c)) + len(list & set(d)) in [0, 1]
     
 
-def witchcraft_res(a, b, c, d):
-    
+def witchcraft_restriction(a, b, c, d):
     list = set([457, 383])
-
     return len(list & set(a)) + len(list & set(b)) + len(list & set(c)) + len(list & set(d)) in [0, 1]
     
 
 
-def taken_malic(a):
+def taken_malicious_magic_requirement(a):
     return 635 in a
 
-def taken_air(a):
+def taken_air_wizardry_requirement(a):
     return 234 in a
 
 
 
-scheduling = Problem()
+def main():
+    scheduling = Problem()
 
-scheduling.addVariable('air', air)
-scheduling.addVariable('adver', adver)
-scheduling.addVariable('core', core)
-scheduling.addVariable('malic', malic)
+    scheduling.addVariable('air', air)
+    scheduling.addVariable('adver', adver)
+    scheduling.addVariable('core', core)
+    scheduling.addVariable('malic', malic)
 
-scheduling.addConstraint(unique_courses, ['air', 'adver', 'core', 'malic'])
-scheduling.addConstraint(good_wizard, ['air', 'adver', 'core', 'malic'])
-scheduling.addConstraint(wizardry_basic, ['air', 'adver', 'core', 'malic'])
-scheduling.addConstraint(witchcraft_res, ['air', 'adver', 'core', 'malic'])
+    scheduling.addConstraint(unique_courses, ['air', 'adver', 'core', 'malic'])
+    scheduling.addConstraint(good_wizard_restriction, ['air', 'adver', 'core', 'malic'])
+    scheduling.addConstraint(wizardry_basic_restriction, ['air', 'adver', 'core', 'malic'])
+    scheduling.addConstraint(witchcraft_restriction, ['air', 'adver', 'core', 'malic'])
 
-scheduling.addConstraint(taken_malic, ['malic'])
-scheduling.addConstraint(taken_air, ['air'])
+    scheduling.addConstraint(taken_malicious_magic_requirement, ['malic'])
+    scheduling.addConstraint(taken_air_wizardry_requirement, ['air'])    
 
-solutions = scheduling.getSolutions()
-for sol in solutions:
-    print(sol)
+    solutions = scheduling.getSolutions()
+    print(len(solutions))
 
-print('\n\n\n')
-print(len(solutions))
+if __name__ == "__main__":
+    main()
